58c9a9f8e88b8c7cd2a7ee117702a9fdc3e315f9
TeklitB <teklitber@gmail.com>
2023-04-21 16:42:34 -0700

    Add project files.

---
+++ HealthCheckWebAPI/Program.cs
@@ -0,0 +1,28 @@
+// Instantiate a WebApplicationBuilder
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Use the builder to create a WebApplication object
+var app = builder.Build();
+
+// Configure the HTTP request pipeline (the middleware).
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Run the app
+app.Run();
